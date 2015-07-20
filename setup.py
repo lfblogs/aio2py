7e1623b8df85b1c50710895024aba3691d9de6af
Liu Fei <13701242710@163.com>
2015-07-20 09:18:46 +0800

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='aio2py',
-    version='1.0.1',
+    version='1.1.0',
     packages=['aio2py', 'aio2py.db', 'aio2py.api', 'aio2py.app', 'aio2py.conf', 'aio2py.http', 'aio2py.utils',
               'aio2py.required', 'aio2py.required.aiopg', 'aio2py.required.aiopg.sa', 'aio2py.required.aiohttp',
               'aio2py.required.aiomysql', 'aio2py.required.aiomysql.sa'],
